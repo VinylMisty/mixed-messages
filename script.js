efa91254c6641cd8fba9b0612892d3231d61c6e4
Misty Orpin <vinylmisty@gmail.com>
2023-09-25 11:14:46 -0500

    Fix index code

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const ciders = ['Next up: Hibiscus. A semi-dry, Hibiscus-infused cider, lending a radiant red hue and characteristic berry-packed zip with crisp, clean citrus and alluring floral aromas.', 'Next up: Peach. Sun-kissed cider, reminiscent of biting into that first freshly-picked orchard peach of the season—enchantingly sweet, juicy, and radiant.', 'Next up: Black Lyte Watermelon. A light sweet profile of watermelon enhanced with a small kick of lime, creating a delightful fusion of flavor. Only 90 calories!', 'Next up: Blackberry. A semi-sweet cider with bewitching aromas of wild blackberry bramble and the thrilling promise of adventure.', "Next up: Strawberry. A taste of perfectly ripe strawberries, still warm from the sun. This supremely pleasant cider is bright and tangy-sweet.', 'Next up: Pumpkin Spice. 'Tis the season of sweaters and pumpkin patches. Break out the Pumpkin Spice Cider around the fire pit for a true Autumn experience."];
+const ciders = ['Next up: Hibiscus. A semi-dry, Hibiscus-infused cider, lending a radiant red hue and characteristic berry-packed zip with crisp, clean citrus and alluring floral aromas.', 'Next up: Peach. Sun-kissed cider, reminiscent of biting into that first freshly-picked orchard peach of the season—enchantingly sweet, juicy, and radiant.', 'Next up: Black Lyte Watermelon. A light sweet profile of watermelon enhanced with a small kick of lime, creating a delightful fusion of flavor. Only 90 calories!', 'Next up: Blackberry. A semi-sweet cider with bewitching aromas of wild blackberry bramble and the thrilling promise of adventure.', "Next up: Strawberry. A taste of perfectly ripe strawberries, still warm from the sun. This supremely pleasant cider is bright and tangy-sweet.", "Next up: Pumpkin Spice. 'Tis the season of sweaters and pumpkin patches. Break out the Pumpkin Spice Cider around the fire pit for a true Autumn experience."];
 
-const generateCider = () => Math.floor(Math.random() * ciders.length);
+const getRandomIndex = () => Math.floor(Math.random() * ciders.length);
+
+function generateCider(num) {
+    let index = getRandomIndex();
+    return ciders[index];
+}
 
 console.log(generateCider())
